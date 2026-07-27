53013841ae2abbcac1402de058e57464ac056b1e
Your Name <you@example.com>
2026-07-27 23:19:41 +0530

    prac_3(2_1)

---
+++ prac_3/driver.java
@@ -0,0 +1,27 @@
+public class Driver {
+    public static void main(String[] args) {
+
+        Card[] cards = {
+            new Card("Ace", "Spades"),
+            new Card("Queen", "Hearts"),
+            new Card("King", "Diamonds"),
+            new Card("King", "Diamonds"),   // Duplicate
+            new Card("10", "Clubs")
+        };
+
+        boolean duplicateFound = false;
+
+        for (int i = 0; i < cards.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (cards[i].equals(cards[j])) {
+                    System.out.println("Duplicate found: " + cards[i]);
+                    duplicateFound = true;
+                    break;
+                }
+            }
+
+            if (duplicateFound)
+                break;
+        }
+    }
+}
